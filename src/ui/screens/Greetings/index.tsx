6b534f9ae5184ba96d06aece7f49baff77aecd29
Lucas Farias <lucasfarias.ln@gmail.com>
2026-03-31 22:30:17 -0300

    Enhance Greetings screen with new components and styles for improved UI

--- src/ui/screens/Greetings/index.tsx
+++ src/ui/screens/Greetings/index.tsx
@@ -1,19 +1,59 @@
-import { ImageBackground } from 'react-native';
-import greetingsBg from '@ui/assets/greetings-bg/image.png';
+import { Logo } from '@ui/assets/Logo';
+import { AppText } from '@ui/components/AppText';
+import { ImageBackground, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from './style';
-import Logo from '@ui/assets/Logo';
+import { theme } from '@ui/styles/theme';
 import { AppButton } from '@ui/components/Button';
+import greetingsBg from '@ui/assets/greetings-bg/image.png';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export function Greetings() {
     return (
-        <ImageBackground
-            source={greetingsBg}
-            resizeMode="cover"
-            style={styles.container}
-        >
-            <Logo width={187} height={60} />
+        <>
+            <ImageBackground
+                source={greetingsBg}
+                resizeMode="cover"
+                style={styles.container}
+            >
+                <SafeAreaView style={styles.content} edges={['top']}>
+                    <Logo />
+                </SafeAreaView>
 
-            <AppButton>Criar minha conta</AppButton>
-        </ImageBackground>
+                <LinearGradient
+                    colors={['transparent', 'rgba(0,0,0,0.85)']}
+                    style={styles.gradient}
+                />
+
+                <SafeAreaView style={styles.ctaContainer} edges={['bottom']}>
+                    <AppText
+                        color={theme.colors.white}
+                        weight="semiBold"
+                        size="3xl"
+                        style={styles.heading}
+                    >
+                        Controle sua dieta de forma simples
+                    </AppText>
+
+                    <View style={styles.ctaContent}>
+                        <AppButton>Criar conta</AppButton>
+
+                        <View style={styles.signInContainer}>
+                            <AppText color={theme.colors.white}>
+                                Já tem conta?
+                            </AppText>
+                            <TouchableOpacity>
+                                <AppText
+                                    color={theme.colors.lime[500]}
+                                    weight="medium"
+                                >
+                                    Acesse a sua conta
+                                </AppText>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </SafeAreaView>
+            </ImageBackground>
+        </>
     );
 }
--- src/ui/screens/Greetings/style.ts
+++ src/ui/screens/Greetings/style.ts
@@ -3,7 +3,39 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    content: {
         alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        flex: 1,
+    },
+    heading: {
+        letterSpacing: -0.32,
+        textAlign: 'center',
+        maxWidth: 311,
+    },
+    ctaContainer: {
+        width: '100%',
+        alignItems: 'center',
+    },
+    gradient: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: 300,
+    },
+    ctaContent: {
+        padding: 20,
+        marginTop: 24,
+        width: '100%',
+    },
+    signInContainer: {
+        marginTop: 16,
+        flexDirection: 'row',
+        gap: 4,
+        paddingVertical: 14,
         justifyContent: 'center',
     },
 });
