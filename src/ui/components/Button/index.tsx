b83c2230831f2f6769f2e6838d0798465d842966
Lucas Farias <lucasfarias.ln@gmail.com>
2026-03-31 22:30:35 -0300

    Add expo-linear-gradient dependency and update Logo component for improved rendering

--- src/ui/components/Button/index.tsx
+++ src/ui/components/Button/index.tsx
@@ -1,12 +1,21 @@
 import { Platform, Pressable, PressableProps } from 'react-native';
 import { AppText } from '../AppText';
-import { styles } from './styles';
+import { buttonStyles, ButtonVariants } from './styles';
 
-interface IAppButtonProps extends PressableProps {
-    children: React.ReactNode;
-}
+type IAppButtonProps = Omit<PressableProps, 'disabled'> &
+    Omit<NonNullable<ButtonVariants>, 'disabled'> & {
+        children: React.ReactNode;
+        disabled?: boolean;
+    };
 
-export function AppButton({ children, ...props }: IAppButtonProps) {
+export function AppButton({
+    children,
+    variant,
+    size,
+    disabled,
+    style,
+    ...props
+}: IAppButtonProps) {
     const childElement =
         typeof children === 'string' ? (
             <AppText weight="medium">{children}</AppText>
@@ -16,9 +25,15 @@ export function AppButton({ children, ...props }: IAppButtonProps) {
     return (
         <Pressable
             style={({ pressed }) => [
-                styles.button,
+                buttonStyles({
+                    size,
+                    variant,
+                    disabled: disabled ? 'true' : 'false',
+                }),
                 pressed && Platform.OS === 'ios' && { opacity: 0.7 },
+                typeof style === 'function' ? style({ pressed }) : style,
             ]}
+            disabled={disabled}
             android_ripple={{ color: 'rgba(0, 0, 0, 0.1)', borderless: false }}
             {...props}
         >
--- src/ui/components/Button/styles.ts
+++ src/ui/components/Button/styles.ts
@@ -1,17 +1,53 @@
 import { theme } from '@ui/styles/theme';
-import { StyleSheet } from 'react-native';
+import { createVariants, VariantProps } from '@ui/styles/utils/createVariants';
 
-export const styles = StyleSheet.create({
-    wrapper: {
-        borderRadius: 12,
-        overflow: 'hidden',
-    },
-    button: {
-        paddingVertical: 14,
-        paddingHorizontal: 24,
-        backgroundColor: theme.colors.lime[500],
+export const buttonStyles = createVariants({
+    base: {
         alignItems: 'center',
         justifyContent: 'center',
         borderRadius: 12,
+        overflow: 'hidden',
+    },
+    variants: {
+        variant: {
+            primary: {
+                backgroundColor: theme.colors.lime[500],
+            },
+            secondary: {
+                backgroundColor: theme.colors.gray[300],
+            },
+            ghost: {
+                backgroundColor: 'transparent',
+            },
+            neutral: {
+                backgroundColor: theme.colors.lime['700/5'],
+            },
+        },
+        size: {
+            default: {
+                paddingHorizontal: 24,
+                paddingVertical: 14,
+                minHeight: 52,
+            },
+            icon: {
+                width: 48,
+                height: 48,
+            },
+        },
+        disabled: {
+            true: {
+                opacity: 0.5,
+            },
+            false: {
+                opacity: 1,
+            },
+        },
+    },
+    defaultVariants: {
+        variant: 'primary',
+        size: 'default',
+        disabled: 'false',
     },
 });
+
+export type ButtonVariants = VariantProps<typeof buttonStyles>;
