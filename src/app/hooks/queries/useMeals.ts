79bfce85ffaf1cee6884d69af347c6009a368bcd
Lucas Farias <lucasfarias.ln@gmail.com>
2026-05-29 23:40:06 -0300

    feat: remove mock data from useMeals query function to ensure real data retrieval

--- src/app/hooks/queries/useMeals.ts
+++ src/app/hooks/queries/useMeals.ts
@@ -1,6 +1,5 @@
 import { MealsService } from '@app/services/MealsService';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
-import mockMeals from '@app/mocks/meals.json';
 
 export function useMeals(date: Date) {
     const formattedDate = date.toISOString().split('T')[0];
@@ -10,12 +9,6 @@ export function useMeals(date: Date) {
         queryKey: ['meals', formattedDate],
         placeholderData: keepPreviousData,
         queryFn: async () => {
-            // TODO: remover mock
-            return mockMeals.map((meal) => ({
-                ...meal,
-                createdAt: new Date(meal.createdAt),
-            }));
-
             const { meals } = await MealsService.getMealsByDate(formattedDate);
             return meals;
         },
